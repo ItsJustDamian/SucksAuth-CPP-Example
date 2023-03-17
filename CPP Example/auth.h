da3b94919949ecc40361f5d95163f2104a9df24a
itsjustdamian <damiandw2003@outlook.com>
2023-03-17 20:50:50 +0100

    Added sub type

--- CPP Example/auth.h
+++ CPP Example/auth.h
@@ -9,7 +9,7 @@ namespace SucksAuth
 	// Needs to be called before any other api calls, this will set the default stuff for the api
 	void Initialize(std::string application_id, std::string encryption_key);
 	// Call this to login and get the required tokens (Set expire date ptr to get pretty expire date)
-	bool Login(std::string license_key, std::string* expire_date = nullptr);
+	bool Login(std::string license_key, std::string* expire_date = nullptr, int* sub_type = nullptr);
 	// Must login first, otherwise it will cause false bans
 	std::vector<unsigned char> Download(std::string file_id);
 	// This will ban the user by as many as possible identifiers
--- CPP Example/entry.cpp
+++ CPP Example/entry.cpp
@@ -8,13 +8,15 @@ int main()
 	SucksAuth::Initialize(XorStr("Application_Id"), XorStr("Encryption_Key"));
 
 	std::string license, expires;
+	int sub_type;
 	std::cout << "Enter license key -> ";
 	std::cin >> license;
 
-	if (!SucksAuth::Login(license, &expires))
+	if (!SucksAuth::Login(license, &expires, &sub_type))
 		return -1;
 
 	std::cout << "Cheat expires in -> " << expires;
+	std::cout << "Sub type -> " << sub_type;
 	std::cin.get();
 	return 0;
 }
